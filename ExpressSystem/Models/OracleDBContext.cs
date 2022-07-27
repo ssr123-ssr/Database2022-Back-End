7df984e78c18f02f209d876bf6fa3990f7ead200
DoubleNorth <1508553028@qq.com>
2022-07-27 16:59:40 +0800

    YuRan's Login/Register demo

---
+++ ExpressSystem/Models/OracleDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressSystem.Models
+{
+    public class OracleDBContext:DbContext
+    {
+        public OracleDBContext(DbContextOptions<OracleDBContext> options) : base(options)
+        {
+
+        }
+
+        //这里的就是数据库中的各个表
+        //建议全部大写(包括Model中的各个property) 要不然容易抛奇怪的异常
+        //因为oracle储存时候应该是把所有字符都转成大写
+        public DbSet<User> USERS { get; set; }
+
+        public DbSet<Client> CLIENT { get; set; }   
+    }
+}
---
+++ ExpressSystem/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressSystem.Models
+{
+    /// <summary>
+    /// User模型   储存用户的登录信息  即账号密码角色
+    /// </summary>
+    public class User
+    {
+        [Required(ErrorMessage ="请输入账号")]
+        [RegularExpression("^[0-9]{5,7}$",ErrorMessage ="账号格式错误(5位或7位数字)")]
+        [Display(Name = "账号")]
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "请输入密码")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$", ErrorMessage = "密码格式错误(大小写字母＋数字,长度为8-20)")]
+        [Display(Name = "密码")]
+        public string PASSWORD { get; set; }
+
+        [Required(ErrorMessage = "请选择角色")]
+        [Display(Name = "角色")]
+        public string ROLE { get; set; }
+    }
+
+
+    public enum RoleEnum { 
+        用户,
+        快递员,
+        司机,
+        代收点,
+        转运中心,
+        快递公司
+    }
+}
